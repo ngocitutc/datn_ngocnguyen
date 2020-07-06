676bfcca0ebd5be1ed29a8d2ce505c9e3169f2fb
ngocnguyen <ngoccool1511@gmail.com>
2020-07-06 15:20:30 +0700

    Create user

--- resources/views/admin/user/create.blade.php
+++ resources/views/admin/user/create.blade.php
@@ -15,44 +15,119 @@
             </div>
 
             <div class="content-wrapper" style="background-color: white;">
-                <div class="row m-0 d-flex p15t">
-                    <div class="col-6">
-                        <div class="row form-group">
+                <div class="row form-group m-0 d-flex p20">
+                    <div class="col-12 col-xl-6">
+                        <div class="row">
+                            <div class="col-12 form-control border-0" style="font-size: 23px; margin-bottom: 20px">
+                                Thông tin đăng nhập
+                            </div>
+
+                        </div>
+                        <div class="row">
                             <div class="col-12 form-control col-xl-5 border-0" >
-                                <span>Tên đăng nhập</span>
+                                <span>Mã đăng nhập</span>
                             </div>
                             <div class="col-12 col-xl-7">
-                                <input type="text" class="form-control m5t m5b fs14" name="house_name">
+                                <input type="text" class="form-control m5t m5b fs14" name="email" placeholder="12345678">
                                 <p class="error-message m0" data-error="house_name"></p>
                             </div>
                         </div>
-                        <div class="row form-group">
+                        <div class="row">
                             <div class="col-12 form-control col-xl-5 border-0" >
                                 <span>Mật khẩu</span>
                             </div>
                             <div class="col-12 col-xl-7">
-                                <input type="text" class="form-control m5t m5b fs14" name="house_name">
+                                <input type="text" class="form-control m5t m5b fs14" name="password" placeholder="abc12345">
                                 <p class="error-message m0" data-error="house_name"></p>
                             </div>
                         </div>
-                        <div class="row form-group">
+                        <div class="row">
                             <div class="col-12 form-control col-xl-5 border-0" >
                                 <span>Email</span>
                             </div>
                             <div class="col-12 col-xl-7">
-                                <input type="text" class="form-control m5t m5b fs14" name="house_name">
+                                <input type="text" class="form-control m5t m5b fs14" name="house_name" placeholder="example@gmail.com">
                                 <p class="error-message m0" data-error="house_name"></p>
                             </div>
                         </div>
-                        <div class="row form-group">
+                        <div class="row">
                             <div class="col-12 form-control col-xl-5 border-0" >
                                 <span>Quyền</span>
                             </div>
                             <div class="col-12 col-xl-7">
-                                <input type="text" class="form-control m5t m5b fs14" name="house_name">
+                                <select class="form-control" name="role" id="">
+                                    <option value="1">Lãnh đạo khoa</option>
+                                    <option value="2">Giảng viên</option>
+                                    <option value="3">Sinh viên</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-xl-6">
+                        <div class="row">
+                            <div class="col-12 form-control border-0" style="font-size: 23px; margin-bottom: 20px">
+                                Thông tin cá nhân
+                            </div>
+
+                        </div>
+                        <div class="row">
+                            <div class="col-12 form-control col-xl-5 border-0" >
+                                <span>Mã sinh viên</span>
+                            </div>
+                            <div class="col-12 col-xl-7">
+                                <input type="text" class="form-control m5t m5b fs14" name="email" placeholder="12345678">
                                 <p class="error-message m0" data-error="house_name"></p>
                             </div>
                         </div>
+                        <div class="row">
+                            <div class="col-12 form-control col-xl-5 border-0" >
+                                <span>Tên sinh viên</span>
+                            </div>
+                            <div class="col-12 col-xl-7">
+                                <input type="text" class="form-control m5t m5b fs14" name="password" placeholder="abc12345">
+                                <p class="error-message m0" data-error="house_name"></p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12 form-control col-xl-5 border-0" >
+                                <span>Ngày sinh</span>
+                            </div>
+                            <div class="col-12 col-xl-7">
+                                <input type="text" class="form-control m5t m5b fs14" name="house_name" placeholder="example@gmail.com">
+                                <p class="error-message m0" data-error="house_name"></p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12 form-control col-xl-5 border-0" >
+                                <span>Giới tính</span>
+                            </div>
+                            <div class="col-12 col-xl-7">
+                                <select class="form-control" name="role" id="">
+                                    <option value="1">Nam</option>
+                                    <option value="2">Nữ</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12 form-control col-xl-5 border-0" >
+                                <span>Điện thoại</span>
+                            </div>
+                            <div class="col-12 col-xl-7">
+                                <input type="text" class="form-control m5t m5b fs14" name="house_name" placeholder="example@gmail.com">
+                                <p class="error-message m0" data-error="house_name"></p>
+                            </div>
+                        </div>
+{{--                        <div class="row">--}}
+{{--                            <div class="col-12 form-control col-xl-5 border-0" >--}}
+{{--                                <span>Địa chỉ</span>--}}
+{{--                            </div>--}}
+{{--                            <div class="col-12 col-xl-7">--}}
+{{--                                <textarea type="text" class="form-control m5t m5b fs14" name="house_name" placeholder="example@gmail.com">--}}
+{{--                                    --}}
+{{--                                </textarea>--}}
+{{--                                <p class="error-message m0" data-error="house_name"></p>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
                     </div>
                 </div>
             </div>
