a1bf4b09e668f88baff1e3a140feb651aded1b79
ngocnguyen <ngoccool1511@gmail.com>
2020-07-09 18:38:20 +0700

    edit data

--- resources/views/student/topic_index.blade.php
+++ resources/views/student/topic_index.blade.php
@@ -40,7 +40,7 @@
                             </tr>
                         @empty
                             <tr>
-                                <td colspan="7">Không có dữ liệu</td>
+                                <td class="text-center" colspan="7">Không có dữ liệu</td>
                             </tr>
                         @endforelse
                         </tbody>
