66a50a331620210b8021924b688797fb7d869dab
ngocnguyen <ngoccool1511@gmail.com>
2020-07-14 14:24:27 +0700

    loc du lieu

--- resources/views/admin/user/list_student.blade.php
+++ resources/views/admin/user/list_student.blade.php
@@ -10,35 +10,36 @@
                 </div>
                 <div class="col-6 m15b text-right ml-auto" style="display: inline-flex; justify-content: flex-end;">
                     <form action="{{ route(USER_LIST_STUDENT) }}" method="GET" id="">
-
-                        <div class="m10r">
-                            <select class="form-control" name="class" id="select-role">
-                                <option value="all">Tất cả lớp</option>
-                                <option value="CNTT-1">CNTT-1</option>
-                                <option value="CNTT-2">CNTT-2</option>
-                                <option value="CNTT-3">CNTT-3</option>
-                            </select>
-                        </div>
-                        <div class="m10r">
-                            <select class="form-control" name="period" id="select-role">
-                                <option value="all">Tất cả khoá</option>
-                                <option value="K59">K59</option>
-                                <option value="K58">K58</option>
-                                <option value="K57">K57</option>
-                                <option value="K56">K56</option>
-                                <option value="K55">K55</option>
-                                <option value="K54">K54</option>
-                            </select>
-                        </div>
-                        <div>
-                            <button class="btn btn-primary border-0 text-white m10r" id="export-file" style="min-width: 100px">
-                                Lọc
-                            </button>
-                        </div>
-                        <div>
-                            <button class="btn btn-warning border-0 text-white" id="export-file">
-                                In danh sách
-                            </button>
+                        <div class="row">
+                            <div class="m10r">
+                                <select class="form-control" name="class" id="select-role">
+                                    <option value="all">Tất cả lớp</option>
+                                    <option value="CNTT-1" @if(request()->class == 'CNTT-1') selected @endif>CNTT-1</option>
+                                    <option value="CNTT-2" @if(request()->class == 'CNTT-2') selected @endif>CNTT-2</option>
+                                    <option value="CNTT-3" @if(request()->class == 'CNTT-3') selected @endif>CNTT-3</option>
+                                </select>
+                            </div>
+                            <div class="m10r">
+                                <select class="form-control" name="period" id="select-role">
+                                    <option value="all">Tất cả khoá</option>
+                                    <option value="K59" @if(request()->period == 'K59') selected @endif>K59</option>
+                                    <option value="K58" @if(request()->period == 'K58') selected @endif>K58</option>
+                                    <option value="K57" @if(request()->period == 'K57') selected @endif>K57</option>
+                                    <option value="K56" @if(request()->period == 'K56') selected @endif>K56</option>
+                                    <option value="K55" @if(request()->period == 'K55') selected @endif>K55</option>
+                                    <option value="K54" @if(request()->period == 'K54') selected @endif>K54</option>
+                                </select>
+                            </div>
+                            <div>
+                                <button class="btn btn-primary border-0 text-white m10r" id="export-file" style="min-width: 100px">
+                                    Lọc
+                                </button>
+                            </div>
+                            <div>
+                                <button class="btn btn-warning border-0 text-white" id="export-file">
+                                    In danh sách
+                                </button>
+                            </div>
                         </div>
                     </form>
                 </div>
--- resources/views/admin/user/list_teacher.blade.php
+++ resources/views/admin/user/list_teacher.blade.php
@@ -10,23 +10,25 @@
                 </div>
                 <div class="col-6 m15b text-right ml-auto" style="display: inline-flex; justify-content: flex-end;">
                     <form action="{{ route(USER_LIST_TEACHER) }}" method="GET" id="">
-                        <div class="m10r">
-                            <select class="form-control" name="subject" id="select-role">
-                                <option value="4">Tất cả bộ môn</option>
-                                <option value="1">{{ SUBJECTS[1] }}</option>
-                                <option value="2">{{ SUBJECTS[2] }}</option>
-                                <option value="3">{{ SUBJECTS[3] }}</option>
-                            </select>
-                        </div>
-                        <div>
-                            <button class="btn btn-primary border-0 text-white m10r" id="export-file" style="min-width: 100px">
-                                Lọc
-                            </button>
-                        </div>
-                        <div>
-                            <button class="btn btn-warning border-0 text-white" id="export-file">
-                                In danh sách
-                            </button>
+                        <div class="row">
+                            <div class="m10r">
+                                <select class="form-control" name="subject" id="select-role">
+                                    <option value="4">Tất cả bộ môn</option>
+                                    <option value="1" @if(request()->subject == "1") selected @endif>{{ SUBJECTS[1] }}</option>
+                                    <option value="2" @if(request()->subject == "2") selected @endif>{{ SUBJECTS[2] }}</option>
+                                    <option value="3" @if(request()->subject == "3") selected @endif>{{ SUBJECTS[3] }}</option>
+                                </select>
+                            </div>
+                            <div>
+                                <button class="btn btn-primary border-0 text-white m10r" id="export-file" style="min-width: 100px">
+                                    Lọc
+                                </button>
+                            </div>
+                            <div>
+                                <button class="btn btn-warning border-0 text-white" id="export-file">
+                                    In danh sách
+                                </button>
+                            </div>
                         </div>
                     </form>
                 </div>
